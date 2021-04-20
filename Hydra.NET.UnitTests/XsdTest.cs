66112b98325b1eaf1269aa4d6b80a66e4387efec
Matt Hoffmeister <matt.hoffmeister@outlook.com>
2021-04-20 12:45:58 -0500

    Add additional XSD types

---
+++ Hydra.NET.UnitTests/XsdTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Hydra.NET.UnitTests
+{
+    public static class XsdTest
+    {
+        [Theory]
+        [InlineData(Xsd.Boolean, "xsd:boolean")]
+        [InlineData(Xsd.DateTime, "xsd:dateTime")]
+        [InlineData(Xsd.Decimal, "xsd:decimal")]
+        [InlineData(Xsd.Double, "xsd:double")]
+        [InlineData(Xsd.Float, "xsd:float")]
+        [InlineData(Xsd.Integer, "xsd:integer")]
+        [InlineData(Xsd.String, "xsd:string")]
+        public static void AccessField_Multiple_ReturnsExpectedValue(
+            string fieldValue, string expectedValue)
+        {
+            // Assert
+
+            Assert.Equal(expectedValue, fieldValue);
+        }
+    }
+}
