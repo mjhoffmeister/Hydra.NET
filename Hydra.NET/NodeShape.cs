53aca43bbbcc190fc69f630f3ebb54d2e3315c52
Matt Hoffmeister <matt.hoffmeister@outlook.com>
2021-06-27 20:01:07 -0500

    Start test for node shape

---
+++ Hydra.NET/NodeShape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Hydra.NET
+{
+    /// <summary>
+    /// A shape adding constraints to a node (class.)
+    /// See https://www.w3.org/TR/shacl/#node-shapes for more info.
+    /// </summary>
+    public class NodeShape
+    {
+        public NodeShape(Uri targetClass, params PropertyShape[] propertyShapes)
+        {
+            PropertyShapes = propertyShapes;
+            TargetClass = targetClass;
+        }
+
+        /// <summary>
+        /// The target class of the node shape.
+        /// </summary>
+        [JsonPropertyName("targetClass")]
+        public Uri TargetClass { get; }
+
+        /// <summary>
+        /// The node shape's <see cref="PropertyShape"/>s.
+        /// </summary>
+        [JsonPropertyName("property")]
+        public IEnumerable<PropertyShape> PropertyShapes { get; }
+    }
+}
